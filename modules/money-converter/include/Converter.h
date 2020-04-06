26b3c37f8803529640fec258f343c909abfda473
Anorak52 <siber56@yandex.ru>
2020-04-06 13:19:30 +0300

    Fix converter.h

--- modules/money-converter/include/Converter.h
+++ modules/money-converter/include/Converter.h
@@ -1,81 +1,54 @@
 // Copyright 2020 Brazhnikov Eugene
 
-#ifndef MODULES_CONVECTOR_LENGTH_INCLUDE_CONVERTER_H_
-#define MODULES_CONVECTOR_LENGTH_INCLUDE_CONVERTER_H_
+#ifndef MODULES_MONEY_CONVERTER_INCLUDE_CONVERTER_H_
+#define MODULES_MONEY_CONVERTER_INCLUDE_CONVERTER_H_
 
 class MoneyConverter {
 public:
-    double rubdollar(double& curr, int  WhichOne)
-    {
-        if (curr >= 0 && WhichOne == 0) // RUB to EUR
-        {
+    double rubdollar(const double& curr, int  WhichOne){
+        if (curr >= 0 && WhichOne == 0){  //RUB to EUR 
             return curr / 77.73;
-        }
-        else if (curr >= 0 && WhichOne == 1) //EUR to RUB
-        {
+        }else if (curr >= 0 && WhichOne == 1){  //EUR to RUB 
             return curr * 77.73;
-        }
-        else
+        }else
             return -1;
     }
 
-    double rubeur(double& curr, int WhichOne)
-    {
-        if (curr >= 0 && WhichOne == 0)
-        {
+    double rubeur(const double& curr, int WhichOne){
+        if (curr >= 0 && WhichOne == 0){
             return curr / 85.73;
-        }
-        else if (curr >= 0 && WhichOne == 1)
-        {
+        }else if (curr >= 0 && WhichOne == 1) {
             return curr * 85.73;
-        }
-        else
+        }else
             return -1;
     }
 
-    double rubJPY(double& curr, int WhichOne)
-    {
-        if (curr >= 0 && WhichOne == 0)
-        {
+    double rubJPY(const double& curr, int WhichOne){
+        if (curr >= 0 && WhichOne == 0){
             return curr / 71.40;
-        }
-        else if (curr >= 0 && WhichOne == 1)
-        {
+        }else if (curr >= 0 && WhichOne == 1){
             return curr * 71.40;
-        }
-        else
+        }else
             return -1;
     }
 
-    double rubSEK(double& curr, int WhichOne)
-    {
-        if (curr >= 0 && WhichOne == 0)
-        {
+    double rubSEK(const double& curr, int WhichOne){
+        if (curr >= 0 && WhichOne == 0){
             return curr / 78;
-        }
-        else if (curr >= 0 && WhichOne == 1)
-        {
+        } else if (curr >= 0 && WhichOne == 1){
             return curr * 78;
-        }
-        else
+        } else
             return -1;
     }
 
-    double rubUAH(double& curr, int WhichOne)
-    {
-        if (curr >= 0 && WhichOne == 0)
-        {
+    double rubUAH(const double& curr, int WhichOne){
+        if (curr >= 0 && WhichOne == 0){
             return curr / 2.79;
-        }
-        else if (curr >= 0 && WhichOne == 1)
-        {
+        } else if (curr >= 0 && WhichOne == 1){
             return curr * 2.79;
-        }
-        else
+        } else
             return -1;
-        
     }
-
 };
 
 #endif
